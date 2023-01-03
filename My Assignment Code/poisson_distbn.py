4362be097815003673468a6471edc43ee2850050
Aniket Khanchi <108761529+aniket-khanchi@users.noreply.github.com>
2023-01-03 19:33:36 +0000

    question 1 completed

--- My Assignment Code/poisson_distbn.py
+++ My Assignment Code/poisson_distbn.py
@@ -5,3 +5,17 @@ from scipy.stats import poisson
 k = np.arange(0, 17)
 
 print(k)
+
+pmf = poisson.pmf(k, mu=7)
+pmf = np.round(pmf, 5)
+
+print(pmf)
+
+for val, prob in zip(k,pmf):
+    print(f"k-value {val} has probability = {prob}")
+
+plt.plot(k, pmf, marker='o')
+plt.xlabel('k')
+plt.ylabel('Probability')
+
+plt.show() 
